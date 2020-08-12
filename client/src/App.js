9af68c820c2645c36d1d279a797084574e17895a
Gamygams1234 <gamyburgos@gmail.com>
2020-08-12 14:52:24 -0700

    Making the reducer and adding the actions

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import AppNavbar from "./components/AppNavbar";
 import ShoppingList from "./components/ShoppingList";
 import "./App.css";
-
+import { Provider } from "react-redux";
+import { store } from "./store";
 function App() {
   return (
-    <div className="App">
-      <AppNavbar />
-      <ShoppingList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <ShoppingList />
+      </div>
+    </Provider>
   );
 }
 
