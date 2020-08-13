66e7996eaabdb69190fffe5f16e0b8ca644d3eea
Gamygams1234 <gamyburgos@gmail.com>
2020-08-12 23:23:06 -0700

    Adding the get items off the server and the item loadi

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,14 +1,9 @@
 import { v4 as uuid } from "uuid";
-import { GET_ITEMS, ADD_ITEM, DELETE_ITEM } from "../actions/types";
+import { GET_ITEMS, ADD_ITEM, DELETE_ITEM, ITEMS_LOADING } from "../actions/types";
 
 const initialState = {
-  items: [
-    { id: uuid(), name: "Eggs" },
-    { id: uuid(), name: "Cheese" },
-    { id: uuid(), name: "Milk" },
-    { id: uuid(), name: "Tomatoes" },
-    { id: uuid(), name: "Kale" },
-  ],
+  items: [],
+  loading: false,
 };
 
 export default function (state = initialState, action) {
@@ -16,6 +11,8 @@ export default function (state = initialState, action) {
     case GET_ITEMS:
       return {
         ...state,
+        items: action.payload,
+        loading: false,
       };
     case DELETE_ITEM:
       return {
@@ -26,6 +23,11 @@ export default function (state = initialState, action) {
       return {
         ...state,
         items: [action.payload, ...state.items],
+      };
+    case ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true,
       };
     default:
       return state;
